680f122f315e9a9d01d0e2ddb52f45982a4afa07
mahmoudkhateep7-arch <mahmoudkhateep7@gmail.com>
2026-04-01 12:16:23 +0200

    commit 4

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,18 @@
 import LastImg from './three_pepsi.png'
-export default function FooterC(){
-  return(
-    <div className='flex justify-center items-center pb-10 pt-4 bg-famber-400'>
-      <img className='w-25' src={LastImg} alt="three pepsi" />
+import { motion } from 'motion/react'
+export default function FooterC() {
+  return (
+    <div className=' pb-10 pt-4'>
+      <motion.div
+        initial={{ x: "-140px" }}
+        viewport={{ once: true }}
+        whileInView={{ x: 0 }}
+
+        transition={{ duration: 0.5 }}
+        className='flex justify-center items-center  bg-famber-400'
+      >
+        <img className='w-25' src={LastImg} alt="three pepsi" />
+      </motion.div>
     </div>
   )
 }
